8778a49aa34038b939cb9860ec2b59bcc906d22a
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2022-03-18 21:33:42 +0000

    Add project files.

---
+++ AutomationTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("AutomationTesting")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16fd5f2d-bea8-4e51-8ddb-e24b7053a7de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomationTesting/SampleCode.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AutomationTesting
+{
+    [TestClass]
+    public class SampleCode
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
