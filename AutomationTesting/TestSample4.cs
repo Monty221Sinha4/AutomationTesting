ac7ed9faeb6eabdb566659765eb3e551dbec98aa
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2022-03-24 09:26:01 +0000

    I have added more test samples to the code. I have used Nunit and Selenium to write the tests!.

---
+++ AutomationTesting/TestSample4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+namespace AutomationTesting
+{
+    [TestClass]
+    public class TestSample4
+    {
+        [TestMethod]
+        public void SampleTest2()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("Start Maximed");
+            IWebDriver webDriver = new ChromeDriver(options);
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(30));
+            webDriver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement textField = webDriver.FindElement(By.Name("q"));
+            textField.SendKeys("Automation testing");
+            IWebElement searchButton = webDriver.FindElement(By.CssSelector("Input[value='Google Search']"));
+            searchButton.Click();
+            webDriver.Dispose();
+
+        }
+    }
+}
